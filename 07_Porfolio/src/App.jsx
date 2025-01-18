1b476c11685393fc4c430fea070094781749955e
NilimaSardar <snilima181@gmail.com>
2025-01-18 15:23:27 +0545

    made footer

--- 07_Porfolio/src/App.jsx
+++ 07_Porfolio/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import Projects from './components/Projects/Projects'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/contact" element={<Contact/>}/>
     </Routes>
+    <Footer/>
     </>
   )
 }
